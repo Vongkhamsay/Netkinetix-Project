6e340164d1c7c69dd6b3ac6fa2cbdf3af15603ba
Jerry Vongkhamsay <jvongkhamsay@hotmail.com>
2017-12-07 15:38:26 -0600

    checkin

--- Netkinetix/Controllers/HomeController.cs
+++ Netkinetix/Controllers/HomeController.cs
@@ -12,10 +12,56 @@ namespace Netkinetix.Controllers
     {
         private NetkinetixContext db = new NetkinetixContext();
 
+        // This is a GET action (Grabs all the data)
         public ActionResult Index()
         {
-            var a = db.SiteEvent.Add(new SiteEvent { SeId = 1, SeTitle = "First Test", SeActive = true, SeDescription = "test", SeEndDate = new DateTime(1999, 09, 01, 21, 34, 00), SeLocation = "Some location", SeStartDate = new DateTime(1999, 09, 01, 21, 34, 00), SeURL = "URL of some sort"  });
-            db.SaveChanges();
+            //.ToList just list all the data
+            return View(db.SiteEvent.ToList());
+
+        }
+        // This is a POST action
+        [HttpPost]
+        public ActionResult Index(string data)
+        {
+            var created = false;
+            ViewBag.Submitted = false;
+
+            if (HttpContext.Request.RequestType == "POST")
+            {
+                ViewBag.Submitted = true;
+                // Sets values to variables that has come back from the form
+                var seTitle = Request.Form["title"];
+                var seStartDate = Request.Form["startDate"];
+                var seEndDate = Request.Form["endDate"];
+                var seLocation = Request.Form["location"];
+                var seDescription = Request.Form["description"];
+                var seURL = Request.Form["url"];
+                var seActive = Request.Form["active"];
+
+                // Sets model properties with variables set above
+                var a = db.SiteEvent.Add(new SiteEvent
+                {
+                    SeTitle = seTitle,
+                    SeStartDate = DateTime.Parse(seStartDate),
+                    SeEndDate = DateTime.Parse(seEndDate),
+                    SeLocation = seLocation,
+                    SeDescription = seDescription,
+                    SeURL = seURL,
+                    SeActive = true
+                });
+                // Save 
+                db.SaveChanges();
+                created = true;
+            }
+            // 
+            if (created)
+            {
+                ViewBag.Message = "Event was created successfully.";
+            }
+            else
+            {
+                ViewBag.Message = "There was an error while creating the event.";
+            }
             return View(db.SiteEvent.ToList());
 
         }
